41c756f9649dbd37227cedd9d9ff422dcbc067d1
Vladimir Timofeev <vovkasm@gmail.com>
2018-01-28 19:52:33 +0300

    initial commit copy of mock-socket, it is great library, also some bugs...

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import buble from 'rollup-plugin-buble';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+  entry: 'src/index.js',
+  sourceMap: 'inline',
+  plugins: [buble(), resolve({ jsnext: true, main: true }), commonjs()],
+  targets: [
+    { dest: 'dist/mock-socket.cjs.js', format: 'cjs' },
+    { dest: 'dist/mock-socket.js', format: 'umd', moduleName: 'Mock' },
+    { dest: 'dist/mock-socket.amd.js', format: 'amd', moduleName: 'Mock' },
+    { dest: 'dist/mock-socket.es.js', format: 'es' }
+  ]
+};
